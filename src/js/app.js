8591b137caeb7b2e99cc848f25cd0fd28adf7527
cristian mosquera <cmosqbone1@gmail.com>
2021-01-22 23:15:29 +0100

    section services finish and cards

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.header .hamburger');
+const mobile_menu = document.querySelector('.header .list');
+// const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+// const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
